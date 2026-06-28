dee4a04b1d43e8aad388c4c08f2cf91c86491338
sanjay mani <sanjaymani9443@gmail.com>
2026-06-28 23:55:57 +0530

    DAY 1

---
+++ dowhile.java
@@ -0,0 +1,11 @@
+public class dowhile{
+    public static void main(String[]argu)
+    {
+        int i=-5;
+        do{
+            System.out.println(i);
+            i++;
+
+        }while(i<=5);
+    }
+}
