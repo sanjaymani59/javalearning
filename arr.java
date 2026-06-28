514a81473ea3f0283600079c182c684d26521c52
sanjay mani <sanjaymani9443@gmail.com>
2026-06-28 22:11:47 +0530

    my first github java

---
+++ arr.java
@@ -0,0 +1,19 @@
+public class arr {
+
+    public static void main(String[]args){
+            String[] playlist = new String[4];
+
+            playlist[0]=" pollatha boomi";
+            playlist[1]="Kandaa Vara Sollunga ";
+            playlist[2]="Unmai Orunaal Vellum";
+            playlist[3]="most life lesson songs for current mindset";
+
+            System.out.println(playlist[0]);
+            System.out.println(playlist[1]);
+            System.out.println(playlist[2]);
+            System.out.println(playlist[3]);
+
+
+    }
+    
+}
