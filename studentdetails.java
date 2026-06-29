4b783c8066d81003ba84d0f9a10351f996feb701
sanjay mani <sanjaymani9443@gmail.com>
2026-06-29 12:11:56 +0530

    studentdetial code

--- studentdetails.java
+++ studentdetails.java
@@ -22,7 +22,7 @@ public class studentdetails {
         char studentFee=sc.next().charAt(0);
 
         System.out.println("Enter student grade:");
-        char studentGrade=sc.varCharAt(0);
+        char studentGrade= sc.
 
         //display data
 
