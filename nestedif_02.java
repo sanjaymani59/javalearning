42e80b03a9fdd0335fd5674fcbdf439cb9d704d5
sanjay mani <sanjaymani9443@gmail.com>
2026-07-07 23:52:54 +0530

    java nestedif_02.java

---
+++ nestedif_02.java
@@ -0,0 +1,19 @@
+public class nestedif_02 {
+
+    public static void main(String[] args) {
+        
+        char attendance = 's';
+
+        if(attendance =='p'){
+
+            System.out.println("present");
+            if(attendance == 's'){
+            System.out.println("on-duty");
+                    }        }
+        else{
+            System.out.println("absent");
+        }
+
+    }
+    
+}
