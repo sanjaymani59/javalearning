fc54391c775a0ec9a451389d5115d3bb732a63e0
sanjay mani <sanjaymani9443@gmail.com>
2026-07-11 21:13:14 +0530

    method code

---
+++ method.java
@@ -0,0 +1,40 @@
+public class method {
+    public static void main(String[] args) {
+
+     
+        System.out.println( add(10,10)); 
+
+        /*  int res =add(10,10);
+        res=res*10;
+        System.out.println(res);
+        */
+
+         System.out.println("c is printed");
+
+       
+    }
+    /* void not using parameters or argumentes 
+
+     public static void add() {  // within add(no parameters)
+        int a =10;
+        int b=10;
+        int c = a+b ;
+        System.out.println(c);
+    }
+     */
+
+     // parameter using void as int add (int a.int b)
+
+    public static int add( int a,int b) {
+
+        int c=a+b;
+        return c;
+    
+        
+    }
+
+
+
+
+
+}
