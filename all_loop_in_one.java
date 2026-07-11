e995ba47a8a828adcbbf5db9056f020071f654ba
sanjay mani <sanjaymani9443@gmail.com>
2026-07-11 15:52:08 +0530

    java all_loop_in_one.java

---
+++ all_loop_in_one.java
@@ -0,0 +1,23 @@
+public class all_loop_in_one {
+    public static void main(String[] args) {
+        int n =5;
+        int sum = 0;
+        int i=5;
+
+      /*   while (i<=n) {
+            sum += i;
+            i++;          
+        }
+            /* */
+        System.out.println("number:"+sum);
+
+        do{
+            sum =+ 1;
+            i++;
+        
+        }while(i<=n);
+
+    
+    }
+}
+
---
+++ nnumfact.java
@@ -0,0 +1,15 @@
+public class nnumfact {
+    public static void main(String[] args) {
+        int n =5;
+        int fact  =1;
+            
+        for( int i=1;i<=n;i++){
+            fact *= i;
+        }
+
+
+            System.out.println("sum of n number:"+fact);
+        }
+    }
+
+    
