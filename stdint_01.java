214cedc446b928a6f4be51c37ff53a2a1e42e35e
sanjay mani <sanjaymani9443@gmail.com>
2026-07-12 21:56:38 +0530

    class codes all

---
+++ stdint_01.java
@@ -0,0 +1,20 @@
+public class stdint_01{
+    String name;
+
+}
+    // instance variable 
+ class Main{
+
+    public static void main(String[]args){
+        stdint_01 s1= new stdint_01();
+        stdint_01 s2=new stdint_01();
+
+        s1.name ="sam";
+        s2.name="jay";
+
+        System.out.println(s1.name);
+        System.out.println(s2.name);
+    }
+
+    
+}
---
+++ stdvar_01.java
@@ -0,0 +1,14 @@
+public class stdvar_01 {
+    static String college="ABC College";
+}
+ class Main{
+    public static void main(String[] args) {
+        stdvar_01 s1=new stdvar_01();
+        stdvar_01 s2=new stdvar_01();
+
+        System.out.println(s1.college);
+        System.out.println(s2.college);
+        
+
+    }
+}
