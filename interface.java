7db3c5729368192f667b2288dcaa2d5d72eb4ed8
sanjay mani <sanjaymani9443@gmail.com>
2026-07-15 21:37:41 +0530

    interface code

---
+++ interface.java
@@ -0,0 +1,26 @@
+interface father {
+
+   abstract void call();
+
+   abstract void talk();
+    
+}
+
+class son implements father {
+
+    public void call(){
+        System.out.println("calling");
+    }
+
+    public void talk(){
+        System.out.println("talking");
+    }
+    public static void main(String[] args) {
+        son ob=new son();
+        ob.call();
+        ob.talk();
+    }
+}  ff
+
+
+  
