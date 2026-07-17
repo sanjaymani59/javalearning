50c49b36d96e5f55161d2bb00c50c345d15eb13d
sanjay mani <sanjaymani9443@gmail.com>
2026-07-17 21:17:22 +0530

    data_type

---
+++ data_type.java
@@ -0,0 +1,20 @@
+class data_type{
+    public static void main(String[] args) {
+        
+    
+
+    String s= Integer.toString(55);
+    System.out.println(s);
+
+
+    String x=Integer.toBinaryString(555);
+    System.out.println(x);
+
+
+    int y =Integer.valueOf(x);
+    System.out.println(y);
+
+
+
+    }
+}
