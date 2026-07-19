66c4ce36a6378bb84690969d5bb7cfc0b4c107f7
sanjay mani <sanjaymani9443@gmail.com>
2026-07-19 23:53:25 +0530

    java codes of

---
+++ q1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+
+class q1 {
+
+    public static void main(String[] args) {
+        Scanner u=new Scanner(System.in);
+        String name= u.nextLine();
+        int age =u.nextInt();
+        u.nextLine();
+        String city=u.nextLine();
+
+        System.out.println("My name is:"+name);
+        System.out.println("My  age is:"+age);
+        System.out.println("My city is:"+city);
+
+    }
+    
+}
---
+++ userinput.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class userinput {
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        System.out.println("Enter a;");
+        int a=obj.nextInt();
+        System.out.println("Enter b:");
+        
+       
+        int b=obj.nextInt();
+       
+        System.out.println(a+b);
+        
+    }
+    
+}
