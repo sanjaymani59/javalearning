f466a131fc19225602da89277d2b642bff1c7ccf
sanjay mani <sanjaymani9443@gmail.com>
2026-07-20 23:30:27 +0530

    rain or not code

---
+++ rain.java
@@ -0,0 +1,17 @@
+ class rain {
+    public static void main(String[] args) {
+        
+        boolean rain =true;
+
+        if(rain){
+        System.out.println("take an a umberlla");
+
+        }
+        else{
+
+        System.out.println("It is your wise");
+        }
+      
+    }
+    
+}
