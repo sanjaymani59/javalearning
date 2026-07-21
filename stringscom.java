89cf8702f58665dec3864e85d15355bf40f2d05f
sanjay mani <sanjaymani9443@gmail.com>
2026-07-21 22:21:39 +0530

    string comparing using .equles

---
+++ stringscom.java
@@ -0,0 +1,12 @@
+class stringscom {
+
+    public static void main(String[] args) {
+        
+        String f1=new String("hello");
+        String f2 =new String("hello");
+
+
+        System.out.println(f1.equals(f2));
+    }
+    
+}
