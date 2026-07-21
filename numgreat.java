fe00d24c83210dbde7eb0da09ae8ebd0c2815d57
sanjay mani <sanjaymani9443@gmail.com>
2026-07-21 21:57:23 +0530

    my code is litte under stanting

---
+++ numgreat.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+
+class numgreat {
+
+    public static void main(String[] args) {
+           
+        Scanner sc=new Scanner(System.in);
+
+        int  num1=sc.nextInt();
+        int num2=sc.nextInt();
+
+        if(num1==num2){
+            System.out.println("num 1 is geater");
+        }
+            else{
+            System.out.println("num2 is geater");
+        }
+        
+}
+}
