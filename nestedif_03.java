18822ed5117a06070a0cb09d562537fd09288bdc
sanjay mani <sanjaymani9443@gmail.com>
2026-07-22 23:38:54 +0530

    java nestedif_03.java

---
+++ nestedif_03.java
@@ -0,0 +1,15 @@
+class nestedif_03 {
+        public static void main(String[] args) {
+            
+            boolean kks=true;
+            boolean rrss=false;
+
+        
+        if(kks==true){
+            System.out.println("open");
+        }if(rrss==true){
+                System.out.println("not open");
+        }
+    }     
+    
+}
