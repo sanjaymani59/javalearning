2fd282dd3ba495fb3658948aaf9eaa9193766b40
sanjay mani <sanjaymani9443@gmail.com>
2026-07-22 23:28:15 +0530

    continues_break

---
+++ continues_break.java
@@ -0,0 +1,15 @@
+
+public class continues_break {
+    public static void main(String[] args) {
+        for (int i=0;i<=20;i++)
+        {
+            if(i%2==0){
+                 continue;
+                /* break */
+            }
+            System.out.println(i+"");
+        }
+    }
+
+     
+}
