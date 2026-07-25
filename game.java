7df805b56939014585f651c3f00d89182bbbea9d
sanjay mani <sanjaymani9443@gmail.com>
2026-07-25 20:09:38 +0530

    get user input

---
+++ game.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class game { 
+    public static void main(String[] args) {
+
+        Scanner obj=new Scanner(System.in);
+         int schore =obj.nextInt();
+      
+
+        if(schore<=50){
+            System.out.println("you need to improve");}
+
+            if(schore>=50 && schore<=60){
+                System.out.println("god job"); }
+                if(schore>=70){
+                    System.out.println("excelent perfomance");}
+    
+            }
+        }
+                   
+    
+
