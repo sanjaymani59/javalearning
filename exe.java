fae2d8c3543b8aff836845fdc6a8298bfa8b0e6c
sanjay mani <sanjaymani9443@gmail.com>
2026-07-26 12:55:08 +0530

    total the number

--- exe.java
+++ exe.java
@@ -1,12 +1,31 @@
 
-import java.util.Scanner;
+ import java.util.Scanner;
 public class exe {
     public static void main(String[]args){
-        int[] Score =new int[5];
+        /* int[] Score =new int[5]; */
 
         Scanner sc=new Scanner(System.in);
 
-        Score[0]=sc.nextInt();
+        int [] score=new int[5];
+        int total=0;
+
+        for( int i=0;i<5;i++){
+            System.out.print("Enter Num"+(i+1)+":");
+            score[i]=sc.nextInt();
+            total += score[i];
+
+
+        }
+        System.out.println("Total Score="+total);
+
+        
+
+
+
+sc.close();
+
+
+       /*  Score[0]=sc.nextInt();
 
         Score[1]=sc.nextInt();
 
@@ -16,10 +35,9 @@ public class exe {
 
         Score[4]=sc.nextInt();
 
-        System.out.println(Score[0]+Score[2]+Score[3]+Score[4]+Score[1]);
-
-
-
-    }
-    
+        System.out.println(Score[0]+Score[2]+Score[3]+Score[4]+Score[1]); */
 }
+} 
+
+
+
