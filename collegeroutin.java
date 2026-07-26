818b72ba794cab9ac20c887f29565e189ebcd3cf
sanjay mani <sanjaymani9443@gmail.com>
2026-07-26 11:03:06 +0530

    college routin code using for loop

---
+++ collegeroutin.java
@@ -0,0 +1,15 @@
+public class collegeroutin {
+    public static void main(String[] args) {
+        String [] days ={"monday","tuesday","wednesday","thursday","friday"};
+
+        for(int i=0;i<days.length;i++){
+            System.out.println("Days:"+days[i]);
+            System.out.println("morning: going to college");
+            System.out.println("9 to 4 : attending class");
+            System.out.println("Evening:coming back to hostal");
+            System.out.println("-----------------------------------");
+        }
+
+    }
+    
+}
