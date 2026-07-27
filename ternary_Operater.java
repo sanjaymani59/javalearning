389c489ea872977b68fcc83c7020453e027996e4
sanjay mani <sanjaymani9443@gmail.com>
2026-07-27 18:05:24 +0530

    ternary_Operater more examples

--- ternary_Operater.java
+++ ternary_Operater.java
@@ -1,12 +1,40 @@
 public class ternary_Operater{
     public static void main(String[] args) {
-        
-  
 
-    int age=20;
+
+        
+        /* to find positive and negative
+        
+        int num =-56;
+
+        
+        String rus= (num/2==0)?"it is positive":"it is negative";
+
+        System.out.println(rus);
+         */
+        
+        
+        /* check even or odd
+        
+        
+        int num=4;
+
+
+            String result=(num %2==0)? "it is even":"it is odd";
+            System.out.println(result);
+ */
+
+
+
+
+
+  /*  check greater or not
+  
+  
+  int age=20;
 
         String result= (age<20)? "it is correct":"it is not correct";
 
-        System.out.println(result);
+        System.out.println(result); */
     }
 }
