665c1573cda3d0877e76d807e1862b9288cab197
sanjay mani <sanjaymani9443@gmail.com>
2026-07-27 19:39:13 +0530

    using_all _example_for_loop_concepts

---
+++ for_loop_wE.java
@@ -0,0 +1,107 @@
+public class for_loop_wE {
+    public static void main(String[] args) {
+
+  /*   //   1. Print numbers from 1 to 10 — for loop
+
+    for ( int i=0;i<11;i++){
+        System.out.println(i);
+    }
+       */
+      
+   /*  //   2. Print numbers from 10 to 1— for loop
+
+
+    System.out.println("hello number from 10 to 1");
+    for (int i=10;i>=1;i--){
+       
+        System.out.println(i);
+    } */
+
+        /* // 3. Print even numbers from 1 to 20
+        System.out.println("Even numbers");
+        for(int i=2;i<=20;i+=2)
+        {
+            System.out.println(i);
+        }
+
+        // 4. odd nubers from 1 to20
+            System.out.println("odd numbers");
+            for(int i=1;i<=20;i+=2){
+                System.out.println(i);cls
+
+            } */
+
+
+        // 5. Sum of numbers from 1 to 10
+
+        /* int sum=0;
+
+        for(int i=0;i<=10;i++){
+
+            // System.out.println(i);
+
+            sum=sum+1;
+        }
+        System.out.println("sum"+sum); */
+      
+
+
+     /*   6. Multiplication table
+
+
+        int n=5;
+
+        for(int i=1;i<10;i++){
+            System.out.println(n+"X"+i+"=" +(n*i));
+
+        } */
+      
+
+        /*     7. Factorial of a number
+
+            int n=5;
+
+            int fact=1;
+
+            for (int i=1;i<=n;i++){
+
+                fact=fact*i;
+           
+            }
+
+            System.out.println("factorial::" +fact);
+
+ */
+
+            // 8. Print squares from 1 to 10
+      
+        /* for(int i=2;i<=10;i++){
+            System.out.println(i+"squres:"+(i*i));
+        } */
+      
+      
+  /*   //   9. Sum of even numbers
+
+    int sum=0;
+
+    for (int i=2;i<=10;i+=2){
+
+        sum =sum+i;
+
+       
+    } System.out.println(sum);
+       */
+      
+      
+      
+      
+      
+        /*  for(int i=0;i<=5;i++);
+        {
+            System.out.println("hello for loop concept");
+
+        } */
+
+
+    }   
+}
