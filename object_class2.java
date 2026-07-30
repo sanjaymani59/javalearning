c3e5693d24f2360dbb1bc36bf317fd1ff7e55173
sanjay mani <sanjaymani9443@gmail.com>
2026-07-30 22:26:27 +0530

    prime

--- object_class2.java
+++ object_class2.java
@@ -1,7 +1,48 @@
  public class object_class2 {
    public static void main(String[] args) {
+
+        int num=10;
+        boolean prime=true;
+
+        for(int i=2;i<num;i++){
+
+            if(num%i==0){
+                prime=false;
+                break;
+            }
+
+        }if(prime){
+            System.out.println("it is prime");
+        }else{
+            System.out.println("not a prime");
+        }
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
-        int num=12345;
+  /*       int num=12345;
         int sum=0;
 
     while(num!=0){
@@ -9,42 +50,11 @@
             sum=sum+digit;
             num=num/10;
     }System.out.println(+sum);
-}}
+}} */
     
 
       
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
+       
         /*  int num=121;
         int original=num;
         int revers=0;
