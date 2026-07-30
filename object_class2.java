ff932bff38191898c1e3a0e350e101e52124de34
sanjay mani <sanjaymani9443@gmail.com>
2026-07-30 19:44:36 +0530

    sum of digit

--- object_class2.java
+++ object_class2.java
@@ -1,8 +1,51 @@
-public class object_class2 {
-    public static void main(String[] args) {
+ public class object_class2 {
+   public static void main(String[] args) {
+    
+        int num=12345;
+        int sum=0;
+
+    while(num!=0){
+            int digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+    }System.out.println(+sum);
+}}
     
 
-        int num=121;
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+        /*  int num=121;
         int original=num;
         int revers=0;
 
@@ -18,8 +61,7 @@ public class object_class2 {
         System.out.println("it is a palindrom");
     }   else{
         System.out.println("it is not a palidrom");
-    }
+    } */
 
     
-}
-}
+
