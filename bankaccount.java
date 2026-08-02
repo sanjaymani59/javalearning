38fc8053d716ff47ac9d0c35b0aeb3979e10ed1e
sanjay mani <sanjaymani9443@gmail.com>
2026-08-02 21:37:33 +0530

    bankaccount

---
+++ bankaccount.java
@@ -0,0 +1,28 @@
+class bankaccount {
+
+    String name;
+    double balance;
+
+    void deposite(int amount){
+       balance +=amount;
+    //    System.out.println("Balance:"+balance);
+    }
+    void display(){
+        System.out.println("Name:"+name);
+        System.out.println("Balance:"+balance);
+    }
+    public static void main(String[] args) {
+        
+        bankaccount ac=new bankaccount();
+        ac.name="jayam";
+        ac.balance=5000.45;
+
+
+        ac.deposite(6000);
+
+
+        ac.display();
+    }
+    
+}
+    
