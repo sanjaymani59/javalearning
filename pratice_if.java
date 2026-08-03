2edc0001cd290b8e9615b8ccc5086a3575d3944c
sanjay mani <sanjaymani9443@gmail.com>
2026-08-03 22:19:50 +0530

    pos,neg,zero

---
+++ pratice_if.java
@@ -0,0 +1,17 @@
+public class pratice_if {
+    public static void main(String[] args) {
+        int num=0;
+        
+            if(num>0){
+                System.out.println("it is positive number");
+            }  else if(num<0){
+                    System.out.println("it is negative number");
+                    }else{
+                        System.out.println( "it is zero ");
+                    }
+        }
+
+      
+    }
+    
+
