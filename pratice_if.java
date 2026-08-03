5da7a9076bd5ef6b0cb0a00368583353eba7904f
sanjay mani <sanjaymani9443@gmail.com>
2026-08-03 22:38:11 +0530

    pratice_leap_year

--- pratice_if.java
+++ pratice_if.java
@@ -1,6 +1,56 @@
 public class pratice_if {
     public static void main(String[] args) {
-        int num=0;
+
+        int a=2024;
+
+        if(a%4==0){
+            if(a%100==0){
+                if(a%400==0){
+                    System.out.println("it is Leap Year");
+                }else{
+                    System.out.println("it is not  leap year");
+                }
+            }else{
+                System.out.println("it a leap year");
+            }
+        }else{
+            System.out.println("it is not a leap year");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* 
+            int a=50;
+            int b=60;
+            int c=40;
+
+            if(a>=b && a>=c){
+                System.out.println("a is greater");
+            }else if(b>=a && b>=c){
+                System.out.println("B is greater");
+            }else{
+                
+//             } */
+// System.out.println("C is greater");
+// ==============================
+
+
+        /* int num=0;
         
             if(num>0){
                 System.out.println("it is positive number");
@@ -8,7 +58,7 @@ public class pratice_if {
                     System.out.println("it is negative number");
                     }else{
                         System.out.println( "it is zero ");
-                    }
+                    } */
         }
 
       
