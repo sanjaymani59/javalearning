65d884010bb4abadc5206a6b3b482b57357187d0
sanjay mani <sanjaymani9443@gmail.com>
2026-08-09 23:53:48 +0530

    mark

--- nestedif_02.java
+++ nestedif_02.java
@@ -1,8 +1,46 @@
 public class nestedif_02 {
 
     public static void main(String[] args) {
+
+
+            int mark=85;
+
+
+
+            if(mark<85){
+                System.out.println("Wanted improvement");
+            }else if(mark>= 85){
+                System.out.println("ok, good try for full mark");
+               
+            }else if(mark==100){
+                System.out.println("great ");
+            }else{
+                System.out.println("give your best");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        char attendance = 's';
+        /* char attendance = 's';
 
         if(attendance =='p'){
 
@@ -12,7 +50,7 @@ public class nestedif_02 {
                     }        }
         else{
             System.out.println("absent");
-        }
+        } */
 
     }
     
