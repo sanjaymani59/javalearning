d59f052df053e753b6509d902579b2e3cbabe199
sanjay mani <sanjaymani9443@gmail.com>
2026-08-10 00:10:23 +0530

    even or odd

--- hi.java
+++ hi.java
@@ -1,7 +1,28 @@
 public class hi {
     public static void main(String[] args) {
-        for (int i=1;i<5;i=i+1){
-        System.out.println("hello");}
+        
+    int num=54;
+
+    if(num%2==0){
+        System.out.println("it is even");
+    }else{
+        System.out.println("it not even");
     }
-    
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+    /* public static void main(String[] args) {
+        for (int i=1;i<5;i=i+1){
+        System.out.println("hello");} */
+   
+
