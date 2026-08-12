bb4114bdd55e8343a5563f6c142e31da5c96db07
sanjay mani <sanjaymani9443@gmail.com>
2026-08-13 00:07:11 +0530

    static

---
+++ static1.java
@@ -0,0 +1,15 @@
+public class static1 {
+    static int a=4;
+    static int b;
+    static{
+        System.out.println("Im in static block");
+        b=a*5;
+    }
+    public static void main(String[] args) {
+        static1 s=new static1();
+
+        System.out.println(static1.a+" "+static1.b);
+
+    }
+    
+}
