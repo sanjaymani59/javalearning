46df6e601157fbed7ca291367508c8737356b54d
sanjay mani <sanjaymani9443@gmail.com>
2026-08-15 02:09:10 +0530

    static

---
+++ Innerthreelarge.java
@@ -0,0 +1,67 @@
+public class Innerthreelarge {
+    static class Test{
+            String name;
+            public Test (String name){
+                this.name=name;
+            }
+        }
+        public static void main(String[] args) {
+            Test a=new Test("samaa");
+            Test b =new Test("jayammm");
+
+            System.out.println(a.name);
+            System.out.println(b.name);
+            
+        }
+        
+        
+            
+        }
+    
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /* public static void main(String[] args) {
+        int a=45;
+        int b=34;
+        int c =98;
+        if(a>b && a>c){
+            System.out.print("a is gratreer");
+        }   
+        else{
+             System.out.println("hello");
+        }
+
+    }} */
+
+            
+
