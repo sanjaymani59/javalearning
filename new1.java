5c9cc79e7d1f99db3a7e3d71d5cd91c35fb668e4
sanjay mani <sanjaymani9443@gmail.com>
2026-08-16 02:27:35 +0530

    enen or odd

---
+++ new1.java
@@ -0,0 +1,12 @@
+public class new1 {
+    public static void main(String[] args) {
+        int num =5;
+        if (num>0){
+            System.out.println("pos");
+        }else if(num<0){
+            System.out.println("neg");
+        }else{
+            System.out.println("zero");
+        }
+    }
+}
