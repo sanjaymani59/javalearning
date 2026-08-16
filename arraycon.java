9793ad0f43628ba68cf1435466ad3bf5a5b53217
sanjay mani <sanjaymani9443@gmail.com>
2026-08-17 00:51:22 +0530

    array super con

---
+++ arraycon.java
@@ -0,0 +1,20 @@
+public class arraycon {
+    public static void main(String[] args) {
+        int [] num={45,50,9,45,35,34};
+       /* System.out.println(num[4]);
+        System.out.println(num[3]);
+        System.out.println(num[2]);
+        System.out.println(num[1]);
+        System.out.println(num[5]); */
+         int sum=0;
+        for (int i=0;i<num.length;i++)
+        {
+            sum =sum+num[i];
+
+            System.out.println(sum);
+        }
+        
+        
+    }
+    
+}
