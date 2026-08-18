7af45cf23f1f24c4a765a473f531b0f2e2959ba7
sanjay mani <sanjaymani9443@gmail.com>
2026-08-18 23:05:17 +0530

    error

---
+++ cons.java
@@ -0,0 +1,21 @@
+public class cons {
+    String name;
+    int age;
+
+
+    public cons(String name){
+        this.name=name;
+        this.age=128;
+        
+    }public cons(String name, int age ){
+        this.name=name;
+        this.age=age;
+    }
+    public static void main(String[] args) {
+        cons l=new cons("heoop o");
+        cons ll=new cons("helllo aa",45);
+    }
+    
+    
+
+}
