c802ecb8911c274a28285ab0604cfe427e5d9cfa
sanjay mani <sanjaymani9443@gmail.com>
2026-08-27 00:40:00 +0530

    average

--- arraycon.java
+++ arraycon.java
@@ -1,21 +1,32 @@
 public class arraycon {
     public static void main(String[] args) {
-        int [] num={45,50,9,45,35,34};
-       /* System.out.println(num[4]);
-        System.out.println(num[3]);
-        System.out.println(num[2]);
-        System.out.println(num[1]);
-        System.out.println(num[5]); */
-         int sum=0;
-        for (int i=0;i<num.length;i++)
-        {
-            
-            sum =sum+num[i];
+    //     int [] num={45,50,9,45,35,34};
+    //    /* System.out.println(num[4]);
+    //     System.out.println(num[3]);
+    //     System.out.println(num[2]);
+    //     System.out.println(num[1]);
+    //     System.out.println(num[5]); */
+    //      int sum=0;
 
-            System.out.println(sum);
-        }
+    //     for (int i=0;i<num.length;i++)
+    //     {
+            
+    //         sum =sum+num[i];
+
+    //         System.out.println(sum);
+        // }
         
+
+        int [] num={10,30,49,54,34};
+        int sum =0;
+        for (int i=0;i<num.length;i++){
+            sum=sum+num[i];
+
         
+         }
+         float average=(float)sum/num.length;
+
+         System.out.println(average);
     }
     
 }
--- loopcon.java
+++ loopcon.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 
 class loopcon{
     public static void main(String[] args) {
-        int [] [] a ={
+        Scanner in =in.Scnn
+
+        int [] a =new int[5];
+       /*  System.out.println(10);
+        System.out.println(20);
+        System.out.println(30);
+        System.out.println(40);
+        System.out.println(50); */
+
+
+
+
+
+       /*  int [] [] a ={
             {1,2,3},
             {4,5,6},
             {7,8,9}
@@ -9,9 +23,9 @@ class loopcon{
 
         for(int i=0;i<3;i++){
             for (int j=0;j<3;j++){
-                System.out.println(a[i][j]+"");
+                System.out.print(a[i][j]+"");
             }
             System.out.println();
-        }
+        } */
     }
 }
