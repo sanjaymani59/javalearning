b657db5888ae76f7006556b437677292b5ea427f
sanjay mani <sanjaymani9443@gmail.com>
2026-08-28 01:56:09 +0530

    largest and smallest

--- arraycon.java
+++ arraycon.java
@@ -17,16 +17,35 @@ public class arraycon {
         // }
         
 
-        int [] num={10,30,49,54,34};
-        int sum =0;
-        for (int i=0;i<num.length;i++){
-            sum=sum+num[i];
+    //     int [] num={10,30,49,54,34};
+    //     int sum =0;
+    //     for (int i=0;i<num.length;i++){
+    //         sum=sum+num[i];
 
         
-         }
-         float average=(float)sum/num.length;
+    //      }
+    //      float average=(float)sum/num.length;
 
-         System.out.println(average);
+    //      System.out.println(average);
+
+
+    // 8. Find Largest and Smallest
+
+
+    int [] num ={10,20,39,49,56};
+    int Largest =num[0];
+    int Smallest=num[0];
+
+    for (int i=1;i<num.length;i++){
+        if(num[i]>Largest){
+            Largest=num[i];
+
+        }
+        if(num[i]<Smallest){
+            Smallest=num[i];
     }
-    
+    }
+    System.out.println("largest"+Largest);
+    System.out.println("smallest"+Smallest);
+}
 }
