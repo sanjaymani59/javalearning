6d03783c0ddef27a0430a1593aa37420bd27fe16
sanjay mani <sanjaymani9443@gmail.com>
2026-09-04 01:36:34 +0530

    simple

--- method_example.java
+++ method_example.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class method_example {
-    void addition(){
+        public static void main(String[] args) {
+            System.out.println("hello");
+
+            Scanner scanner=new Scanner(System.in);
+            System.out.println("enter name");
+
+            String name = scanner.nextLine();
+            System.out.println(name);
+
+            scanner.close();
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*     void addition(){
         int a=10;
         int b=20;
         int c=a+b;
@@ -17,6 +52,6 @@ public static void main(String[] args) {
 
     caller.addition();
     caller.sub(55, 34);
-}
+} */
 
 }
