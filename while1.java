15b2ed49909b1cca0795b53dd6bce9b736b1ba62
sanjay mani <sanjaymani9443@gmail.com>
2026-09-05 22:15:10 +0530

    while loop

---
+++ while1.java
@@ -0,0 +1,11 @@
+public class while1{
+    public static void main(String[] args) {
+        int c=0;
+
+        while(c<=3){
+            System.out.println(c);
+            c++;
+        }
+    }
+    
+}
