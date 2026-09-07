b849e2a2e9c0af34a5a58f316d2583e478fc8b07
sanjay mani <sanjaymani9443@gmail.com>
2026-09-08 01:53:04 +0530

    array

---
+++ arrayexample.java
@@ -0,0 +1,53 @@
+// import java.util.ArrayList;
+
+// public class while1{
+//     public static void main(String[] args) {
+//             int [] num ={3,5,4,2,6,4,6,7,5,4};
+
+
+//             ArrayList<Integer> uniqueList = new ArrayList<>();
+
+//             for (int item: num){
+//                 if(!uniqueList.contains(item)){
+//                     uniqueList.add(item);
+//                 }
+//             }
+
+//             System.out.println("og");
+
+//             for (int n:num) System.out.println(n+" ");
+
+//             System.out.println( "" +uniqueList);
+
+
+public class arrayexample {
+	public static void main(String [] args) {
+		String [] b = {"TN","KL","KA"};
+		for ( String i : b) {
+			System.out.println(i);
+			System.out.println(b[0]);
+		}
+		
+		
+	}
+	
+
+}
+
+
+
+
+
+
+
+
+
+    //     int c=0;
+
+    //     while(c<=3){
+    //         System.out.println(c);
+    //         c++;
+    //     }
+    }
+    
+}
