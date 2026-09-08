3f9f4ad49498bf50d1439acb3eb9996d6d5e374f
sanjay mani <sanjaymani9443@gmail.com>
2026-09-09 00:10:32 +0530

    simple

--- student_01.java
+++ student_01.java
@@ -1,13 +1,18 @@
 
 
 public class student_01 {
-    student_01(){
-    System.out.println("Default constructor called");
+
+//     student_01(){
+//     System.out.println("Default constructor called");
 
 
-}
+// }
 public static void main(String[] args) {
-    student_01 s1 =new student_01();
+    String data ="1,growth,male";
+
+    String a[] =data.split(",");
+//     student_01 s1 =new student_01();
+System.out.println(a[1]);
 }
 
     
