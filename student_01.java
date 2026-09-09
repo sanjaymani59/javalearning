9be7824fb3de96de9fc2836672bf8b51e5726dc2
sanjay mani <sanjaymani9443@gmail.com>
2026-09-10 00:10:10 +0530

    simple java

--- student_01.java
+++ student_01.java
@@ -1,19 +1,38 @@
-
+import java.util.ArrayList;
 
 public class student_01 {
+
+    public static void main(String[] args) {
+        
+    ArrayList<String> names=new ArrayList<>();
+    names.add("hello");
+    names.add("ho");
+    names.add("hi");
+    String str=names.get(2);
+
+    names.set(1, "dare");
+
+    System.out.println("Arrayname:" + names + str);
+
+    }
+
+    
+
 
 //     student_01(){
 //     System.out.println("Default constructor called");
 
 
 // }
-public static void main(String[] args) {
+/* public static void main(String[] args) {
     String data ="1,growth,male";
 
     String a[] =data.split(",");
 //     student_01 s1 =new student_01();
 System.out.println(a[1]);
 }
+ */
 
+    
     
 }
