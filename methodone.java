52584bd313c5bbf62148466a254fc5af0e65238d
sanjay mani <sanjaymani9443@gmail.com>
2026-09-21 23:14:54 +0530

    method one

---
+++ methodone.java
@@ -0,0 +1,56 @@
+// import java.util.jar.Attributes.Name;
+
+
+
+
+
+   /*  static void   boxvolumn(int lenght,int breadth,int width){
+        int vol;
+        vol=lenght*breadth*width ;
+        System.out.println(vol);
+        // return vol;
+    }
+    public static void main(String[] args) {
+
+        boxvolumn(10,3,4);
+        // int volume = boxvolumn(10,3,4);
+            // System.out.println(volume);
+            // return volume; */
+
+    // }
+
+
+     /*  static int   sum(int a,int b){
+        int add ;
+        add =a+b;
+        return add;
+        // System.out.println(add);
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int add=sum(23,45);
+        System.out.println(add);
+        
+
+    } */
+
+
+
+
+       /*  public class methodone {
+    static String welcome(){
+        String name;
+        name ="EAT somosa";
+        return name;
+        // System.out.println(name);
+
+
+        }
+    public static void main(String[] args) {
+
+        welcome();
+        System.out.println(welcome());
+    } */
+}
