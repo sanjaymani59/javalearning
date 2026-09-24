acdbf00774d53568ed5a9c8af9f11983300f0851
sanjay mani <sanjaymani9443@gmail.com>
2026-09-24 22:17:47 +0530

    constructor

--- constructor_01.java
+++ constructor_01.java
@@ -111,7 +111,7 @@ public class constructor_01 {
 
 
 
-class std {
+/* class std {
     String name;
     int age;
 
@@ -145,5 +145,132 @@ public class constructor_01 {
     s2.display();
     s3.display();
     }
+} */
+
+
+    // 6. Using this Keyword ⭐
+    // Sometimes constructor parameter names and variable names are the same.
+
+
+/* class std{
+    String name;
+    int age;
+
+    std(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+
+    void  display(){
+        System.out.println(name);
+        System.out.println(age);
+    }
 }
 
+public class constructor_01 {
+        public static void main(String[] args) {
+            std s =new std("sam", 25);
+            s.display();
+        }
+    
+} */
+
+// 7. Constructor with Calculation
+
+
+/* class rectangle {
+        int length;
+        int width;
+        int area;
+
+        rectangle(int length,int width){
+            this.length=length;
+            this.width=width;
+
+            area=length*width;
+        }
+        void display(){
+            System.out.println("Area:" +area);
+        }
+    
+}
+public class constructor_01 {
+    public static void main(String[] args) {
+        
+  
+        rectangle r=new rectangle(5, 8);
+
+        r.display();
+    }
+    
+} */
+
+
+
+    // 8. Constructor with Multiple Objects
+
+
+/* class Employe {
+        String name;
+        int salary;
+        Employe(String name,int salary){
+           this.name=name;
+           this.salary=salary;
+        }
+        void display(){
+            System.out.println(name +' '+salary);
+        }
+    }
+  
+public class constructor_01 {
+    public static void main(String[] args) {
+        
+ 
+        Employe em= new Employe("sam", 20);
+        Employe em2= new Employe("jam", 21);
+        Employe em3= new Employe("calm", 22);
+
+        em.display();
+        em2.display();
+        em3.display();
+
+
+    }
+} */
+
+
+
+    // 9. Constructor Calling Another Constructor — this()
+
+
+
+
+   class std {
+        String name;
+        int age;
+
+        std(){
+            this("unknown",0);
+        }
+
+        std( String name ,int age){
+            this.name=name;
+            this.age=age;
+        }
+
+        void display(){
+            System.out.println(name+ " " +age);
+        }
+   
+    }
+
+    public class constructor_01 {
+        public static void main(String[] args) {
+            std s=new std();
+            std s1=new std("sam",8);
+
+            s.display();
+            s1.display();
+        }
+        
+    }
